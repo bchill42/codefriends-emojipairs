8c90dafef3b3455816a6693d2238f33630bde1d6
Brandon Hill <brandon@climbalytics.com>
2020-08-18 19:02:07 -0500

    adding setting that allow you to change board size and reset board

--- board.js
+++ board.js
@@ -65,6 +65,34 @@ function selectedSquare() {
 }
 
 function createBoardArray() {
+  console.log(document.getElementsByClassName("square")[0]);
+  document.getElementById("side-container").innerHTML = "Turns 0";
+  totalClicks = 0;
+  if (document.getElementsByClassName("square").length > 1) {
+    let saveFirst = document.getElementsByClassName("square")[0];
+    saveFirst.classList.remove("squarePaired");
+    saveFirst.style.flex = "1 0 13%";
+    // for (i = 0; i > document.getElementsByClassName("square").length; i++) {
+    //   document.getElementsByClassName("square").innerHTML = "";
+    // }
+    document.getElementById("main").innerHTML = "";
+    document.getElementById("main").appendChild(saveFirst);
+  }
+  for (let i = 0; i < boardItems / 2; i++) {
+    let num = Math.floor(Math.random() * Math.floor(numberArray.length));
+    if (!randomNumberArray.includes(num)) {
+      randomNumberArray.push(num);
+      randomNumberArray.push(num);
+    } else {
+      i--;
+    }
+  }
+  let mixedUpNumArray = [];
+  for (let i = 0; i < boardItems; i++) {
+    let num = Math.floor(Math.random() * Math.floor(randomNumberArray.length));
+    mixedUpNumArray.push(randomNumberArray[num]);
+    randomNumberArray.splice(num, 1);
+  }
   let emojiArray = [
     "😅",
     "😄",
@@ -115,30 +143,81 @@ function createBoardArray() {
     document.getElementById("main").appendChild(clone);
   }
   squareContainer[0].remove();
+  if (boardItems === 16) {
+    for (i = 0; i < 16; i++) {
+      document.getElementsByClassName("square")[i].style.flex = "1 0 20%";
+    }
+  }
 }
 
 function boardSettings() {
-  let boardSize = document.getElementById("boardSize");
+  let boardSizeDiv = document.getElementById("boardSizeDiv");
   let numPlayers = document.getElementById("numPlayers");
   let loadBoard = document.getElementById("loadBoard");
-  boardSize.addEventListener("click", setBoardSize);
+  boardSizeDiv.accessKey = 6;
+  console.log(`Initial Board Size Access Key ${boardSizeDiv.accessKey}`);
+  boardSizeDiv.addEventListener("click", setBoardSize);
   numPlayers.addEventListener("click", setNumPlayers);
   loadBoard.addEventListener("click", setLoadBoard);
 }
 
+function changeBoardSize() {
+  console.log(parseInt(this.innerHTML));
+  boardSize = parseInt(this.innerHTML);
+  document.getElementById("boardSizeDiv").accessKey = boardSize;
+  board2.classList.add("selected");
+  board4.classList.remove("selected");
+  board6.classList.remove("selected");
+  document.getElementsByClassName("boardNum")[0].style.display = "none";
+  document.getElementsByClassName("boardNum")[1].style.display = "none";
+  document.getElementsByClassName("boardNum")[2].style.display = "none";
+  console.log(`accesskey = ${boardSize}`);
+  document.getElementById("loadBoard").style.backgroundColor = "#23D028";
+}
+
 function setBoardSize() {
-  console.log("board size");
-  getElementsByClassName("boardNum").style.visibilty = "visible";
+  //   let boardSizeDiv = document.getElementById("boardSizeDiv");
+  //   boardSizeDiv.accessKey = 6;
+  //   console.log(boardSizeDiv.accessKey);
+  boardSize = document.getElementById("boardSizeDiv").accessKey;
+  console.log(`board size, ${boardSize}`);
+  document.getElementsByClassName("boardNum")[0].style.display = "block";
+  document.getElementsByClassName("boardNum")[1].style.display = "block";
+  document.getElementsByClassName("boardNum")[2].style.display = "block";
+  let board2 = document.getElementById("board2");
+  let board4 = document.getElementById("board4");
+  let board6 = document.getElementById("board6");
+  if (boardSize === 2) {
+    board2.classList.add("selected");
+  }
+  if (boardSize === 4) {
+    board4.classList.add("selected");
+  }
+  if (boardSize === 6) {
+    board6.classList.add("selected");
+  }
+  board2.addEventListener("click", changeBoardSize);
+  board4.addEventListener("click", changeBoardSize);
+  board6.addEventListener("click", changeBoardSize);
 }
 
 function setNumPlayers() {
   console.log("number of players");
+  numPlayers = 2;
 }
 
 function setLoadBoard() {
   console.log("load board");
-  //   document.getElementById("big-contanier").innerHTML = "";
-  //   createBoardArray();
+  console.log(
+    `accesskey, ${document.getElementById("boardSizeDiv").accessKey}`
+  );
+  document.getElementById("loadBoard").innerHTML = "Reset Board";
+  document.getElementById("loadBoard").style.backgroundColor = "#c4cce1";
+  boardSize = document.getElementById("boardSizeDiv").accessKey;
+  boardItems = Math.pow(boardSize, 2);
+  squaresRemaining = boardItems;
+  document.getElementById("big-contanier").style.visibility = "visible";
+  createBoardArray();
 }
 
 const numberArray = [
@@ -178,29 +257,15 @@ let board = [];
 let clickDisable = false;
 let picked = [];
 let pickedBack = [];
-let boardSize = 2;
-let boardItems = Math.pow(boardSize, 2);
 let totalClicks = 0;
-let squaresRemaining = boardItems;
 let count = 0;
 let randomNumberArray = [];
+let numPlayers = 1;
+let boardSize;
+let boardItems = Math.pow(boardSize, 2);
+let squaresRemaining = boardItems;
 
-for (let i = 0; i < boardItems / 2; i++) {
-  let num = Math.floor(Math.random() * Math.floor(numberArray.length));
-  if (!randomNumberArray.includes(num)) {
-    randomNumberArray.push(num);
-    randomNumberArray.push(num);
-  } else {
-    i--;
-  }
-}
-
-let mixedUpNumArray = [];
-for (let i = 0; i < boardItems; i++) {
-  let num = Math.floor(Math.random() * Math.floor(randomNumberArray.length));
-  mixedUpNumArray.push(randomNumberArray[num]);
-  randomNumberArray.splice(num, 1);
-}
-
+document.getElementById("big-contanier").style.visibility = "hidden";
 boardSettings();
-createBoardArray();
+// setLoadBoard();
+// createBoardArray();
